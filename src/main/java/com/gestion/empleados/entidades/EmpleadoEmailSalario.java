9f6c9bcbdc62ed20bf55102b42474d0a5db3d7c6
Andres-Gz <afgm.913@hotmail.com>
2024-05-15 17:02:17 -0500

    | TASK | - Se ajusta archivo para no tener codesmells

--- src/main/java/com/gestion/empleados/entidades/EmpleadoEmailSalario.java
+++ src/main/java/com/gestion/empleados/entidades/EmpleadoEmailSalario.java
@@ -14,15 +14,8 @@ public class EmpleadoEmailSalario {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Double getSalario() {
         return salario;
     }
 
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
 }
